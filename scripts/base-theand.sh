f37a33013689ff5ad662948a25b99ef2a17f5a30
Heesang Chae <theand@gmail.com>
2014-01-31 22:02:29 +0900

    my apt package

---
+++ scripts/base-theand.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing Base Packages for theand"
+
+# Update
+sudo apt-get update
+
+# Install base packages
+sudo apt-get install -y dos2unix debconf-utils w3m reportbug sendmail-bin
+
