ac508fabc1361d18793594394063b3972212cad9
Heesang Chae <theand@gmail.com>
2014-02-16 12:43:27 +0900

    add language locale ko

--- scripts/base-theand.sh
+++ scripts/base-theand.sh
@@ -6,7 +6,7 @@ echo ">>> Installing Base Packages for theand"
 sudo apt-get update
 
 # Install base packages
-sudo apt-get install -y dos2unix debconf-utils w3m reportbug sendmail-bin unzip
+sudo apt-get install -y dos2unix debconf-utils w3m reportbug sendmail-bin unzip language-pack-ko-base
 
 #sudo apt-get upgrade -y
 
