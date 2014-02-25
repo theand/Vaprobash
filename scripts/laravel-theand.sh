b08aec3c1921bf8ce9568722dcec2a9aa04584d8
Heesang Chae <theand@daumcorp.com>
2014-02-25 20:43:16 +0900

    스크립트 수정

--- scripts/laravel-theand.sh
+++ scripts/laravel-theand.sh
@@ -2,7 +2,7 @@
 
 echo ">>> Installing Laravel Installer"
 
-curl -L http://laravel.com/laravel.phar 
-chmod +x laravel.phar 
+curl -LO http://laravel.com/laravel.phar
+chmod +x laravel.phar
 mv laravel.phar /usr/local/bin/laravel
 
--- scripts/vim-theand.sh
+++ scripts/vim-theand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/^Bundle 'altercation/vim-colors-solarized'/\"Bundle 'altercation/vim-colors-solarized'/" /home/vagrant/.vimrc
-sed -i "s/^colorscheme solarized/\"colorscheme solarized" /home/vagrant/.vimrc
+sed -i "s/^Bundle 'altercation\/vim-colors-solarized'/\"Bundle 'altercation\/vim-colors-solarized'/" /home/vagrant/.vimrc
+sed -i "s/^colorscheme solarized/\"colorscheme solarized/" /home/vagrant/.vimrc
