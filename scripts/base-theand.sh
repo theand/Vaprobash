3a42fad5665b076980f52e8826e8f0862e1cba6d
Heesang Chae <theand@gmail.com>
2014-09-30 13:23:09 +0900

    add launchpad-keys

--- scripts/base-theand.sh
+++ scripts/base-theand.sh
@@ -2,10 +2,13 @@
 
 echo ">>> Installing Base Packages for theand"
 
+sudo add-apt-repository ppa:nilarimogard/webupd8
+
 # Update
 sudo apt-get update
 
 # Install base packages
-sudo apt-get install -qq dos2unix debconf-utils w3m reportbug sendmail-bin language-pack-ko-base tree ack-grep
+sudo apt-get install -qq dos2unix debconf-utils w3m reportbug sendmail-bin language-pack-ko-base tree ack-grep launchpad-getkeys
 
 #sudo apt-get upgrade -y
+sudo launchpad-getkeys
