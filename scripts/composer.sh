a1050a3961c4e96cf4dc2394d55415bddc09f4f2
Heesang Chae <theand@gmail.com>
2014-10-05 02:09:35 +0900

    fix laravel .profile

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -28,10 +28,10 @@ if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
         sudo rm installer
 
         # Add an alias that will allow us to use composer without timeout's
-        printf "\n# Add an alias for sudo\n%s\n# Use HHVM when using Composer\n%s\n" \
-        "alias sudo=\"sudo \"\n" \
-        "alias composer=\"hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false /usr/local/bin/composer\"\n" \
-        "alias laravel=\"hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false ~/.composer/vendor/bin/laravel\"\n" \
+        printf "\n\n# Add an alias for sudo\n%s\n# Use HHVM when using Composer\n%s\n#Use HHVM when using laravel\n%s\n\n" \
+        "alias sudo=\"sudo \"" \
+        "alias composer=\"hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false /usr/local/bin/composer\"" \
+        "alias laravel=\"hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 -v Eval.Jit=false ~/.composer/vendor/bin/laravel\"" \
         >> "/home/vagrant/.profile"
 
         # Resource .profile
@@ -70,7 +70,7 @@ if [[ ! -z $COMPOSER_PACKAGES ]]; then
             # Ensure COMPOSER_HOME variable is set. This isn't set by Composer automatically
             printf "\n\nCOMPOSER_HOME=\"/home/vagrant/.composer\"" >> /home/vagrant/.profile
             # Add composer home vendor bin dir to PATH to run globally installed executables
-            printf "\n# Add Composer Global Bin to PATH\n%s\n" 'export PATH=$PATH:$COMPOSER_HOME/vendor/bin' >> /home/vagrant/.profile
+            printf "\n# Add Composer Global Bin to PATH\n%s\n\n" 'export PATH=$PATH:$COMPOSER_HOME/vendor/bin' >> /home/vagrant/.profile
 
             # Source the .profile to pick up changes
             . /home/vagrant/.profile
