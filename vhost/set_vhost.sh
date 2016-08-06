9f7236b7f3d275cd3b6193ddb236fd03b6685ec1
Heesang Chae <theand@gmail.com>
2016-08-06 15:31:21 +0900

    update vhost conf

---
+++ vhost/set_vhost.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /etc/apache2/sites-available/
+sudo ln -s /vagrant/vhost/book-new.dev.conf
+sudo ln -s /vagrant/vhost/clean-center.dev.conf
+#sudo ln -s /vagrant/vhost/hr.dev.conf
+sudo ln -s /vagrant/vhost/hr-dkb.dev.conf
+sudo ln -s /vagrant/vhost/hr-dks.dev.conf
+sudo ln -s /vagrant/vhost/hr-dkt.dev.conf
+sudo ln -s /vagrant/vhost/ui.dev.conf
+sudo ln -s /vagrant/vhost/synonym.conf
+sudo ln -s /vagrant/vhost/arttoy.dev.conf
+sudo ln -s /vagrant/vhost/arttoy-admin.dev.conf
+
+sudo a2ensite book-new.dev.conf
+sudo a2ensite clean-center.dev.conf
+#sudo a2ensite hr.dev.conf
+sudo a2ensite hr-dkb.dev.conf
+sudo a2ensite hr-dks.dev.conf
+sudo a2ensite hr-dkt.dev.conf
+sudo a2ensite ui.dev.conf
+sudo a2ensite synonym.conf
+sudo a2ensite arttoy.dev.conf
+sudo a2ensite arttoy-admin.dev.conf
+
+sudo service apache2 restart
